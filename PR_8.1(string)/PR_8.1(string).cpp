2d1ef06ec94d8cdadb70561082f3d627a1bd0a82
ievgen275 <ievgen3435@gmail.com>
2024-03-14 15:28:42 +0200

    first commit

---
+++ PR_8.1(string)/PR_8.1(string).cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int Count(const string s)
+{
+    int k = 0;
+    size_t pos = 0;
+    while ((pos = s.find("SQ", pos)) != string::npos || (pos = s.find("QS", pos)) != string::npos)
+    {
+        k++;
+        pos += 2;
+    }
+    return k;
+}
+
+string Change(string& s)
+{
+    size_t pos = 0;
+    while (true)
+    {
+        size_t posSQ = s.find("SQ", pos);
+        size_t posQS = s.find("QS", pos);
+        if (posSQ == string::npos && posQS == string::npos) break;
+        if (posSQ != string::npos && (posQS == string::npos || posSQ < posQS))
+        {
+            s.replace(posSQ, 2, "***"); 
+            pos = posSQ + 3;
+        }
+        else if (posQS != string::npos && (posSQ == string::npos || posQS < posSQ))
+        {
+            s.replace(posQS, 2, "***"); 
+            pos = posQS + 3;
+        }
+    }
+    return s;
+}
+
+
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+    cout << "String contained " << Count(str) << " groups of 'SQ' or 'QS'" << endl;
+    string dest = Change(str);
+    cout << "Modified string (result): " << dest << endl;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_8.1(string)/PR_8.1(string).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string testString = "pp SQ pp QS";
+			Assert::AreEqual(1, Count(testString));
+			Assert::AreEqual(string("pp *** pp ***"), Change(testString));
+		}
+	};
+}
